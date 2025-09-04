b0a1495be539e89f2bf299a89c839deaf49c87df
Stefano Canducci <stefano@canducci.org>
2025-09-04 14:56:38 +0200

    home style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-// @ts-check
 import netlify from "@astrojs/netlify";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -7,4 +7,7 @@ export default defineConfig({
   site: "https://warhammer-rss.netlify.app/",
   adapter: netlify(),
   output: "server",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
